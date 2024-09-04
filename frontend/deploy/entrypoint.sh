ff4e936616dd4b4eb2308cb9cf31d3386a5b0f18
hunsy9 <juniper0917@gmail.com>
2024-09-04 14:44:27 +0900

    action ts

--- frontend/deploy/entrypoint.sh
+++ frontend/deploy/entrypoint.sh
@@ -14,17 +14,16 @@ fi
 
 echo "Using SERVER_NAME: $SERVER_NAME"
 
-if [ -z "$SERVER_NAME" ]; then
-    # SERVER_NAME 환경 변수가 설정되지 않았을 경우 기본값 사용
-    SERVER_NAME="csep.seung.site"
-    echo "SERVER_NAME was not set, using default: $SERVER_NAME"
-fi
+#if [ -z "$SERVER_NAME" ]; then
+#    # SERVER_NAME 환경 변수가 설정되지 않았을 경우 기본값 사용
+#    SERVER_NAME="csep.seung.site"
+#    echo "SERVER_NAME was not set, using default: $SERVER_NAME"
+#fi
 
 # 동적으로 server_name 변경
 sed -i "s/__SERVER_NAME__/$SERVER_NAME/g" /app/deploy/nginx/nginx.conf
 sed -i "s/__SERVER_NAME__/$SERVER_NAME/g" /app/deploy/nginx/ssl_config.conf
 echo "Nginx configuration after substitution:"
-cat /app/deploy/nginx/nginx.conf
 
 # static contents 전송을 위한 CDN 호스트 설정
 cd $DIST
